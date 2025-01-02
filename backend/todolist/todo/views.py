ea78953aa0f94a78abc2c6676bf352ff43d82759
JadeAtyla <jamadigal@gmail.com>
2025-01-03 04:29:58 +0800

    Implement soft delete for Todo items in the API

--- backend/todolist/todo/views.py
+++ backend/todolist/todo/views.py
@@ -9,3 +9,13 @@ from rest_framework.decorators import action
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoListSerializer
     queryset = Todo.objects.filter(deleted=False)
+    
+    def destroy(self, request, pk=None):
+        try:
+            todo = self.get_object()
+            todo.deleted = True
+            todo.save()
+            return Response({"message": "Todo marked as deleted"}, status=status.HTTP_200_OK)
+        except Todo.DoesNotExist:
+            return Response({"message": "Todo not found"}, status=status.HTTP_404_NOT_FOUND)
+
